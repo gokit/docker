980fd363dd0ea113e74ba15ccdb03344591d50e2
Alexander Ewetumo <trinoxf@gmail.com>
2019-01-15 21:28:03 +0800

    UPT: Removed nohup

--- kafka-run.sh
+++ kafka-run.sh
@@ -3,6 +3,6 @@
 export ZOOKEEPER_PEERS='localhost:2181'
 export KAFKA_PEERS='localhost:9092'
 
-nohup bash -c "cd kafka && bin/zookeeper-server-start.sh config/zookeeper.properties &"
+bash -c "cd kafka && bin/zookeeper-server-start.sh config/zookeeper.properties &"
 sleep 3
 bash -c "cd kafka && bin/kafka-server-start.sh config/server.properties &"
