deb265d5b416d2ac817833c0153d4974407b927a
Alexander Ewetumo <trinoxf@gmail.com>
2019-02-04 15:53:40 +0800

    UPT: Update kafka and gcloud versions

--- kafka-setup.sh
+++ kafka-setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-wget http://www.us.apache.org/dist/kafka/0.10.2.2/kafka_2.10-0.10.2.2.tgz  -O kafka.tgz
+wget http://www.us.apache.org/dist/kafka/2.1.0/kafka_2.12-2.1.0.tgz  -O kafka.tgz
 mkdir -p kafka && tar xzf kafka.tgz -C kafka --strip-components 1
 rm -rf kafka.tgz
